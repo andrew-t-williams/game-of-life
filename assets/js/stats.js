6b9cbe02108b7a10782b511c28ddeeb6da0fe600
Andrew Williams <andrew.th.williams@gmail.com>
2021-12-25 21:20:00 -0700

    Refactoring

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -94,3 +94,59 @@ function removeData(chart) {
     });
     chart.update();
 }
+
+function timingStats() {
+    // Calculate timestamps
+    var stat_last = stat_now
+    stat_now = performance.now()
+    stat_framerate_array[stat_frame_count % stat_frame_array_length] = stat_now - stat_last
+    stat_frame_count++
+
+    // Twice every array length calculate and display framerate
+    if (stat_frame_count % (stat_frame_array_length / 2) == 0) {
+        var real_framerate = (1000 * stat_frame_array_length) / stat_framerate_array.reduce(arrayadd, 0)
+        var rounded_real_framerate = Math.round(real_framerate * 1000) / 1000
+        stat_real_framerate.value = rounded_real_framerate
+        var raw_fps_lag = ((stat_ideal_framerate.value * 100) / real_framerate) - 100
+        var rounded_fps_lag = (Math.round((raw_fps_lag) * 1000) / 1000)
+        stat_framerate_diff.value = (rounded_fps_lag > 1000 ? ">1000" : rounded_fps_lag) + "%"
+
+        addData(stat_fps_chart, stat_frame_count, [stat_ideal_framerate.value, real_framerate])
+        var num_visible_data_points = 10
+        if (stat_frame_count > num_visible_data_points * stat_frame_array_length / 2) {
+            removeData(stat_fps_chart)
+        }
+    }
+    // console.log(stat_frame_count)
+    // if (stat_frame_count >= stat_frame_array_length) { stat_frame_count = 0 }
+
+    // Hide certain stats if game is advenced using button
+    if (!running) {
+        stat_real_framerate.value = "--"
+        stat_framerate_diff.value = "--"
+    }
+}
+
+function nonTimingStats() {
+    // Optional statistics
+    if (stats_switch.checked) {
+        var living = countLivingCells(cells)
+        var total = (grid_x_count * grid_y_count)
+        var living_ratio = Math.round((living / total) * 1000) / 10
+
+        stat_living_cells.value = living
+        stat_dead_cells.value = total - living
+        stat_living_ratio.value = living_ratio + "%"
+        if (living > stat_living_cells_max.value) {
+            stat_living_cells_max.value = living
+            stat_living_ratio_max.value = living_ratio + "%"
+            stat_living_ratio_max_chart.data.datasets[0].data = [living, total - living]
+            stat_living_ratio_max_chart.update()
+        }
+
+        stat_living_ratio_chart.data.datasets[0].data = [living, total - living]
+        stat_living_ratio_chart.update()
+
+
+    }
+}
